8091d7f7ea84f110cbcc4b023a34ca2af22cfed7
Your Name <showtimeee>
2017-10-28 04:55:27 -0400

    phil

--- sample.js
+++ sample.js
@@ -1,8 +1,13 @@
-var name; 
+var name=("phil"); 
 var email;
 var username;
 var password;
+const Password;
+var b;
+var a;
+if  (username===) {
+prompt ("Enter your name ");
+alert ("Alert: This is a mismatch alert!!");
+}
 
 
-prompt("Enter your name");
-alert(name);
