32aa920aea20ab9ea5cc7a381a76c64c2f31eee1
Your Name <showtimeee>
2017-11-03 01:25:00 -0400

    phil

---
+++ External.js
@@ -0,0 +1,12 @@
+
+function firstone(){
+ 
+alert("");
+}
+
+
+
+
+function EventHandler(){
+alert(" Auto Lynk ");
+}
--- sample.js
+++ sample.js
@@ -1,13 +1,7 @@
-var name=("phil"); 
-var email;
-var username;
-var password;
-const Password;
-var b;
-var a;
-if  (username===) {
-prompt ("Enter your name ");
-alert ("Alert: This is a mismatch alert!!");
-}
+var a =();
+var b=();
 
+function secondone(){
+alert(" ");
+}
 
